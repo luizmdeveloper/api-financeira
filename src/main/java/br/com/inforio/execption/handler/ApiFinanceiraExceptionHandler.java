dd7a694d36e73e5a32258f52897e273ac1d351a3
luizmdeveloper <luizmariodev@gmail.com>
2019-09-30 22:22:33 -0300

    Adicionando as validações necessárias

--- src/main/java/br/com/inforio/execption/handler/ApiFinanceiraExceptionHandler.java
+++ src/main/java/br/com/inforio/execption/handler/ApiFinanceiraExceptionHandler.java
@@ -1,9 +1,18 @@
 package br.com.inforio.execption.handler;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -15,6 +24,9 @@ import br.com.inforio.service.exception.TransacaoNaoCadastradaException;
 
 @ControllerAdvice
 public class ApiFinanceiraExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@ExceptionHandler({ CategoriaNaoCadastradaException.class })
 	public ResponseEntity<Object> handleCategoriaNaoCadastradaException(CategoriaNaoCadastradaException ex, WebRequest request){
@@ -42,7 +54,28 @@ public class ApiFinanceiraExceptionHandler extends ResponseEntityExceptionHandle
 		
 		return ResponseEntity.badRequest().body(erros);
 	}	
+	
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<Erro> erros = criarListaErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
 		
+	private List<Erro> criarListaErros(BindingResult bindingResult) {
+		List<Erro> erros = new ArrayList<>();
+		
+		for(FieldError fieldError: bindingResult.getFieldErrors()) {
+			System.out.println(">>>>> fieldError " + messageSource.getMessage(fieldError, LocaleContextHolder.getLocale()));
+			String mensagemErroUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemErroDesenvolvedor = fieldError.toString();
+		
+			erros.add(new Erro(mensagemErroUsuario, mensagemErroDesenvolvedor));
+		}
+				
+		return erros;
+	}
+
 	public static class Erro{
 		
 		private String mensagemUsuario;
